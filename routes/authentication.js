ec3ec632086406d894839961ed3d64961712f41b
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-12-17 03:03:02 +0530

    develop user login api endpoint

--- routes/authentication.js
+++ routes/authentication.js
@@ -12,12 +12,36 @@ router.post("/register", async (req, res) => {
     })
     try {
         const savedUser = await newUser.save()
-        res.status(201)
-        res.json(savedUser)
+        res.status(201).json(savedUser)
     }
     catch (error) {
         res.status(500).json(error)
         console.log(error)
+    }
+})
+
+// Login
+router.get("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({
+            username: req.body.username
+        })
+        if(!user) {
+            return user && res.status(401).json("Wrong credentials!")
+        }
+
+        const decryptedPassword = CryptoJS.AES.decrypt(user.password, process.env.ENC_KEY).toString(CryptoJS.enc.Utf8)
+        if(decryptedPassword !== req.body.password) {
+            return res.status(401).json("Wrong credentials!")
+        }
+
+        // user info saved on _doc inside mongodb, so if you send the following line it will send whole document
+        // const { password, ...otherInfo} = user
+        const { password, ...otherInfo} = user._doc
+        res.status(200).json(otherInfo)
+    }
+    catch (error) {
+        res.status(500).json(error)
     }
 })
 
