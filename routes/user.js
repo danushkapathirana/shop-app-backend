1666af7abbc595f5073521a41785b828cec3087a
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-12-17 06:42:30 +0530

    develop find all user account api endpoint

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const router = require("express").Router()
 
 const User = require("../models/User")
-const { verifyTokenAndAuthorization } = require("./verifyToken")
+const { verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken")
 
 // Update
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
@@ -24,6 +24,30 @@ router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
     try {
         await User.findByIdAndDelete(req.params.id)
         res.status(200).json("User has been deleted!")
+    }
+    catch (err) {
+        res.status.json(err)
+    }
+})
+
+// Get user
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        const { password, ...others } = user._doc
+        res.status(200).json({others})
+    }
+    catch (err) {
+        res.status.json(err)
+    }
+})
+
+// Get all user
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    const query = req.query.new
+    try {
+        const users = query ? await User.find().sort({_id: -1}).limit(1) : await User.find()
+        res.status(200).json(users)
     }
     catch (err) {
         res.status.json(err)
